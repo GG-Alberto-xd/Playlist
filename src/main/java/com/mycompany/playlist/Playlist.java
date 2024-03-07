a0375f81d8f1d06a0cf5201aad3b1815f6ff8a94
corte <corte@LENOVOV14G3>
2024-03-07 16:54:15 -0600

    Se realizo un programa que simula una playlist, en este programa solo hay una cancion.

---
+++ src/main/java/com/mycompany/playlist/Playlist.java
@@ -0,0 +1,9 @@
+package com.mycompany.playlist;
+
+public class Playlist {
+
+    public static void main(String[] args) {
+        Musicas p1=new Musicas("Mr. Brownstone", "Appetite For Destruction", "1987", 4);
+        p1.ShowPlaylist();
+        }
+}
